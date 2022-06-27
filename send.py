6c4361cc932d95c6d12547ca4826c01243a497fa
soroush aali bagi <aalibagi.soroush@gmail.com>
2022-06-27 13:36:11 -0600

    Create send.py

---
+++ send.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+import sys
+import argparse
+import readline
+import time
+import string
+import random
+import datetime
+from probe_hdrs import *
+
+def main():
+	while(True):
+		doit("10.1.1.1", 2, 10000)
+		
+def doit(dest, teni, nopa):
+	randchar = random.choice(string.ascii_letters)
+	addr = socket.gethostbyname(dest)
+    	dummy = 720*"$"
+    
+	probe_pkt = Ether(dst='ff:ff:ff:ff:ff:ff', src=get_if_hwaddr('eth0')) / Probe(tenant=teni) / IP(dst=addr) / randchar
+    
+    for i in range(nopa):
+    	try:
+		with open(str(probe_pkt[IP].src)+".s.txt", "a") as myfile:
+    			for i in range(nopa):
+    				probe_pktt = probe_pkt / (str(i+1)) / dummy
+    				sendp(probe_pktt, iface='eth0')
+    				myfile.write(randchar+str(i+1)+","+str(time.time())+"\n")
+	except KeyboardInterrupt:
+		sys.exit()
+
+if __name__ == '__main__':
+    main()
